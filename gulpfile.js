bead6dc081adef3ce729211a379603926476e95c
Ruaidhri Devery <ruaidhri@zymeworks.com>
2014-12-15 13:39:10 -0800

    Setup Gulp build to produce and tests artifacts

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp    = require('gulp'),
+    rename  = require('gulp-rename'),
+    gutil  = require('gulp-util'),
+    uglify  = require('gulp-uglify'),
+    karma = require('karma').server;
+
+gulp.task('build', function () {
+  return gulp.src('pathway.js')
+    .pipe(gulp.dest('build/'))
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('test', function (done) {
+  karmaTest(['build/pathway.js', 'pathway.spec.js'], done);
+});
+
+gulp.task('test-min', function (done) {
+  karmaTest(['build/pathway.min.js', 'pathway.spec.js'], done);
+});
+
+/**
+ * Run karma tests once loading files provided
+ *
+ * @param  {Array<String>}  files Array of file patterns to load
+ * @param  {Function}       done
+ */
+function karmaTest(files, done) {
+  karma.start({
+    browsers: ['Chrome', 'PhantomJS'],
+    files: files,
+    frameworks: ['jasmine'],
+    singleRun: true
+  }, function (exitCode) {
+    gutil.log('Karma has exited with: ' + exitCode);
+    done(exitCode);
+  });
+}
